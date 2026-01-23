5936d3c66bed04f78619252860844f6ff806b738
Charl Christopher M. Peñada <cmpenada@student.apc.edu.ph>
2026-01-23 15:06:13 +0800

    Created a Supabase Client

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,43 +1,21 @@
-<template>
-  <div class="app-wrapper">
-    <h1>Lazy Day</h1>
-    
-    <div class="food-container">
-      <food-item/>
-      <food-item/>
-      <food-item/>
-      <food-item/>
-      <food-item/>
-      <food-item/>
-      
-      <food-item2/>
-      <food-item2/>
-      <food-item2/>
-      <food-item2/>
-      <food-item2/>
-      <food-item2/>
-    </div>
-  </div>
-</template>
-
-<script>
-export default {
-
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabaseClient'
+ 
+const instruments = ref([])
+ 
+async function getInstruments() {
+  const { data } = await supabase.from('instruments').select()
+  instruments.value = data
 }
+ 
+onMounted(() => {
+   getInstruments()
+})
 </script>
-
-<style>
-  .app-wrapper {
-    font-family: 'Arial', sans-serif;
-    text-align: center; 
-    margin-top: 60px;
-  }
-
-  .food-container {
-    max-width: 800px; 
-    margin: 0 auto;  
-    padding: 20px;
-    background-color: #f0f0f0; 
-    border-radius: 10px;
-  }
-</style>
+ 
+<template>
+  <ul>
+    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
+  </ul>
+</template>
