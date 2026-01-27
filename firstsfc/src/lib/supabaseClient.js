3af699dfa6749c593d6d00cca2d102f0105d7de6
Charl Christopher M. Peñada <cmpenada@student.apc.edu.ph>
2026-01-27 02:23:45 +0000

    Added debugger

--- firstsfc/src/lib/supabaseClient.js
+++ firstsfc/src/lib/supabaseClient.js
@@ -1,4 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
+
+// Log the variable to see if it exists (check Console after deploy)
+console.log("DEBUG: Supabase URL is", import.meta.env.VITE_SUPABASE_URL ? "Defined" : "MISSING");
+
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabasePublishableKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
+
 export const supabase = createClient(supabaseUrl, supabasePublishableKey)
